f357c952dcbf660c15547d37c36ee4cf25a2d666
Luke Lovett <lukeharmonlovett@gmail.com>
2022-09-11 08:40:49 -0700

    make tests better

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -5,8 +5,7 @@ import tempfile
 import unittest
 
 import aqt
-from aqt import _run
-from aqt import AnkiApp
+from aqt import AnkiApp, _run
 from aqt.main import AnkiQt
 from aqt.profiles import ProfileManager
 
@@ -59,6 +58,10 @@ class AnkiRunner:
         if app is None:
             raise Exception('Could not create Anki app')
 
+        # The plugin requires a profile for import and only works with the v3 scheduler.
+        aqt.mw.setupProfile()
+        aqt.mw.col.set_v3_scheduler(True)
+
         return app
 
     def stop_anki(self):
@@ -85,46 +88,46 @@ class E2ETest(unittest.TestCase):
         cls.anki_runner = AnkiRunner()
         cls.anki_runner.start_anki()
 
-        from aqt import mw
-        mw.setupProfile()
-        mw.col.set_v3_scheduler(True)
-        cls.mw = mw
-
     @classmethod
     def tearDownClass(cls) -> None:
         cls.anki_runner.stop_anki()
-
-    @staticmethod
-    def fixture_path(filename):
-        return os.path.join(os.path.dirname(__file__), 'fixtures', filename)
-
-    def revlog_records(self):
-        return sorted(
-            # match expected types from self.review_history
-            (sfld, str(rid))
-            for sfld, rid in self.mw.col.db.execute(
-                'select notes.sfld, revlog.id from notes '
-                'inner join cards on cards.nid = notes.id '
-                'inner join revlog on revlog.cid = cards.id'
-            ))
 
     def setUp(self) -> None:
         # Plugin can only be imported after Anki is running.
         import jpdb_anki_import
 
-        self.config = jpdb_anki_import.config.Config(review_file=self.fixture_path('vocabulary-reviews.json'))
-        self.importer = jpdb_anki_import.importer.JPDBImporter(self.config, self.mw)
+        self.config = jpdb_anki_import.config.Config(
+            review_file=self.fixture_path('vocabulary-reviews.json'))
+        self.importer = jpdb_anki_import.importer.JPDBImporter(self.config, aqt.mw)
 
         with open(self.fixture_path('review-history.csv'), 'r') as csvfile:
             self.review_history = sorted(tuple(review) for review in csv.reader(csvfile))
 
+    def tearDown(self) -> None:
+        aqt.mw.col.decks.current().clear()
+
+    @staticmethod
+    def fixture_path(filename):
+        return os.path.join(os.path.dirname(__file__), 'fixtures', filename)
+
+    @staticmethod
+    def review_log():
+        return sorted(
+            # match expected types from self.review_history
+            (sfld, str(rid))
+            for sfld, rid in aqt.mw.col.db.execute(
+                'select notes.sfld, revlog.id from notes '
+                'inner join cards on cards.nid = notes.id '
+                'inner join revlog on revlog.cid = cards.id'
+            ))
+
     def test_import(self):
         """Test importing into a deck without notes."""
         self.importer.run()
-        self.assertEqual(self.review_history, self.revlog_records())
+        self.assertEqual(self.review_history, self.review_log())
 
     def test_import_with_existing_notes(self):
         """Test importing into a deck where there are already notes."""
         self.importer.run()
         self.importer.run()
-        self.assertEqual(self.review_history, self.revlog_records())
+        self.assertEqual(self.review_history, self.review_log())
