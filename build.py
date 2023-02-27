3f033a605a59b339f0477672d64555a23de9e717
KaranveerB <52545097+KaranveerB@users.noreply.github.com>
2023-02-26 19:11:21 -0800

    Add missing manifest.json for building

--- build.py
+++ build.py
@@ -1,16 +1,20 @@
 import os
 import zipfile
 
-addon_name = "jpdb_anki_import.ankiaddon"
-source_dir = "jpdb_anki_import"
+addon_name = 'jpdb_anki_import.ankiaddon'
+source_dir = 'jpdb_anki_import'
 
 # Create a zip file containing all the .py files in the source directory
-with zipfile.ZipFile(addon_name, "w") as addon_zip:
+with zipfile.ZipFile(addon_name, 'w') as addon_zip:
+    addon_zip.write('manifest.json')
     for root, dirs, files in os.walk(source_dir):
         for file in files:
-            if file.endswith(".py"):
+            if file.endswith('.py'):
                 file_path = os.path.join(root, file)
-                arcname = os.path.relpath(file_path, source_dir)
-                addon_zip.write(file_path, arcname)
+                # ensure file is added to root of zip file
+                arc_file_name = os.path.basename(file_path)
+                addon_zip.write(file_path, arc_file_name)
 
-print(f"Addon built successfully")
+addon_path = os.path.abspath(addon_name);
+
+print(f'Add-on built successfully at {addon_path}')
